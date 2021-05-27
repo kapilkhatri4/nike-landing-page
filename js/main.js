f1c047f2f1d21bf0994344a3470817abb0ea34f8
kapilkhatri4 <kapilkhatri4040@gmail.com>
2021-05-27 12:01:58 +0530

    commit

---
+++ js/main.js
@@ -0,0 +1,135 @@
+$('.center').slick({
+  centerMode: true,
+  centerPadding: '0px',
+  slidesToShow: 4,
+  responsive: [
+    {
+      breakpoint: 1140,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        // centerPadding: '40px',
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 1024,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        // centerPadding: '40px',
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        // centerPadding: '40px',
+        slidesToShow: 2,
+      },
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        arrows: true,
+        centerMode: true,
+        // centerPadding: '0px',
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
+
+$('.responsive').slick({
+  dots: true,
+  infinite: false,
+  speed: 300,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true,
+      },
+    },
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2,
+      },
+    },
+    {
+      breakpoint: 575,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
+
+var swiper = new Swiper('.mySwiper', {
+  slidesPerView: 3,
+  spaceBetween: 30,
+  loop: true,
+  loopFillGroupWithBlank: true,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+const menuBtn = document.querySelector('.navbar-toggler');
+const navigation = document.querySelector('.navbar-nav');
+menuBtn.addEventListener('click', function () {
+  navigation.classList.toggle('navactive');
+});
+
+const sliderimg = document.querySelectorAll('.slider-img');
+const mainImg = document.querySelector('.img-1');
+const silde = document.querySelectorAll('.slide');
+const sliderContainer = document.querySelector('.slider');
+const mainContainer = document.querySelector('.main-section');
+const navBar = document.querySelector('.navbar');
+sliderContainer.addEventListener('click', function () {
+  sliderimg.forEach(el => {
+    el.addEventListener('click', function (e) {
+      const sliderSrc = e.target.src;
+      mainImg.src = sliderSrc;
+      mainImg.animate(
+        [
+          {
+            width: 0,
+            transform: 'rotate(30deg)',
+          },
+          {
+            width: '100%',
+            transform: 'rotate(-30deg)',
+          },
+        ],
+        {
+          duration: 500,
+        }
+      );
+      const dataColor = e.target.dataset.color;
+      mainContainer.style.background = dataColor;
+      navBar.style.background = dataColor;
+      navigation.style.background = dataColor;
+      sliderContainer.style.background = dataColor;
+    });
+  });
+});
